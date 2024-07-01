7d2911db2f48f9dcd9930bbac322243401e9a3d7
mohammed3200 <wwyuu799@gmail.com>
2024-07-01 06:01:25 +0200

    :wrench:

--- app/(home)/(tabs)/ads/post.tsx
+++ app/(home)/(tabs)/ads/post.tsx
@@ -3,31 +3,29 @@ import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { Background, Header } from "@/components";
 import { icons } from "@/constants";
+import Card from "./Card";
 
 const post = () => {
   const { image, title, description, period } = useLocalSearchParams();
   return (
     <>
-      <View
-      className="flex-1 bg-primary"
-      >
+      <View className="flex-1 bg-primary">
         <Header
           title={title as string}
           left={{
             icon: icons.arrow,
             onPress: () => router.replace("ads"),
-            size:30,
-            backgroundColor:"#F0F0F5"
+            size: 30,
+            backgroundColor: "#F0F0F5",
           }}
           dark
         />
-        <View
-        className="flex-1"
-        >
-            <Background/>
+        <View className="flex-1">
+          <Background />
+          <Card positions={1} source=""/>
+          <Card positions={0.5} source=""/>
+          <Card positions={0} source=""/>
         </View>
-
-
       </View>
     </>
   );
