beaec2935f9e969f0a9e2f99a49011c6d6af8fcf
mohammed3200 <wwyuu799@gmail.com>
2024-07-01 07:07:02 +0200

    :wrench:

--- app/(home)/(tabs)/ads/post.tsx
+++ app/(home)/(tabs)/ads/post.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { Background, Header } from "@/components";
@@ -8,9 +8,8 @@ import { BlurView } from "expo-blur";
 import { useSharedValue } from "react-native-reanimated";
 
 const post = () => {
-    
-    const { image, title, description, period } = useLocalSearchParams();
-    const photo = (image)?.toString().split(",");
+  const { image, title, description, period } = useLocalSearchParams();
+  const photo = image?.toString().split(",");
   return (
     <>
       <View className="flex-1 bg-primary">
@@ -26,19 +25,33 @@ const post = () => {
         />
         <View className="flex-1">
           <Background />
-          <BlurView
-            intensity={100}
-            style={{ borderRadius: 20 }}
-            className="m-6 h-fit w-fit px-4 py-2 rounded-xl"
+          <ScrollView
+            style={{
+              borderBottomLeftRadius: 55,
+              borderBottomRightRadius: 55,
+              height: undefined,
+            }}
+            contentContainerStyle={{ paddingVertical: 15, height: undefined }}
+            showsVerticalScrollIndicator={false}
           >
-            <Text className="font-DNNextLT text-sm text-gray-200">
-              {description}
-            </Text>
-          </BlurView>
+            <BlurView
+              intensity={100}
+              style={{ borderRadius: 20 }}
+              className="m-6 h-fit w-fit px-4 py-2 rounded-xl"
+            >
+              <Text className="font-DNNextLT text-sm text-gray-200">
+                {description}
+              </Text>
+            </BlurView>
+          </ScrollView>
           <View className="flex-1">
             {image && Array.isArray(photo)
               ? photo.map((item) => (
-                  <Card positions={useSharedValue(1)} source={item} key={item} />
+                  <Card
+                    positions={useSharedValue(1)}
+                    source={item}
+                    key={item}
+                  />
                 ))
               : null}
           </View>
