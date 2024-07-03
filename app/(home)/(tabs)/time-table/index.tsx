a1c267a2b1eec6b1633ac1756b94e3cd4635c5fa
mohammed3200 <wwyuu799@gmail.com>
2024-07-03 16:29:31 +0200

    :wrench:

--- app/(home)/(tabs)/time-table/index.tsx
+++ app/(home)/(tabs)/time-table/index.tsx
@@ -48,16 +48,14 @@ const TimeTable = () => {
   >([]);
 
   const viewableItems = useSharedValue<ViewToken[]>([]);
+  const ref = useRef<ListOfCoursesRefProps>(null);
 
   const handleDayPress = useCallback(
     (day: { code: string; name: string; isActive: boolean }) => {
       setSelectedDay(day.code);
-      setLecturesDay(
-        Data?.LectureDays.filter((item) => selectedDay === item.Day)
-      );
-      // Implement your logic to filter the courses based on the selected day
+      setLecturesDay(Data?.LectureDays.filter((item) => day.code === item.Day));
     },
-    []
+    [Data]
   );
 
   useEffect(() => {
@@ -94,7 +92,14 @@ const TimeTable = () => {
     setRefreshing(false);
   }, [data, error, refetch]);
 
-  const ref = useRef<ListOfCoursesRefProps>(null);
+  useEffect(() => {
+    if (Data) {
+      setLecturesDay(
+        Data.LectureDays.filter((item) => selectedDay === item.Day)
+      );
+    }
+  }, [selectedDay, Data]);
+
 
   const onPressList = useCallback(() => {
     const isActive = ref?.current?.isActive();
@@ -108,193 +113,190 @@ const TimeTable = () => {
   return (
     <AlertNotificationRoot>
       <GestureHandlerRootView className="flex-1">
-        <ScrollView
+        {/* <ScrollView
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
           contentContainerStyle={{ height: "100%" }}
-        >
-          <View className="flex-1">
-            <View
-              style={{
-                flex: 0.25,
+        > */}
+        <View className="flex-1">
+          <View
+            style={{
+              flex: 0.25,
+            }}
+            className="bg-Bg rounded-b-[55px]"
+          >
+            <Header
+              title="جدول المحاضرات"
+              left={{
+                icon: Icons.Dot,
+                onPress: () => navigation.dispatch(DrawerActions.openDrawer()),
               }}
-              className="bg-Bg rounded-b-[55px]"
-            >
-              <Header
-                title="جدول المحاضرات"
-                left={{
-                  icon: Icons.Dot,
-                  onPress: () =>
-                    navigation.dispatch(DrawerActions.openDrawer()),
+              right={{
+                icon: Icons.menuDot,
+                onPress: onPressList,
+              }}
+            />
+            {isLoading ? (
+              <View className="flex-1 items-center justify-center">
+                <Chase size={50} color="rgba(255,255,255,0.5)" />
+              </View>
+            ) : (
+              <View className="flex-1 justify-center px-4 py-3">
+                <Text className="font-DNNextLTB text-lg text-primary">
+                  {" "}
+                  الفصل الحالي : {Data?.SemesterName}
+                </Text>
+                <Text className="font-DNNextLT text-base text-gray-300">
+                  {" "}
+                  الفصل الدراسي الحالي : {Data?.Semester}
+                </Text>
+              </View>
+            )}
+          </View>
+          <View
+            style={{
+              flex: 0.7,
+            }}
+            className="bg-primary"
+          >
+            <CategoriesDay
+              selectedDay={selectedDay}
+              onDayPress={handleDayPress}
+            />
+            {LecturesDay ? (
+              <FlatList
+                data={LecturesDay}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingTop: 40 }}
+                onViewableItemsChanged={({ viewableItems: vItems }) => {
+                  viewableItems.value = vItems;
                 }}
-                right={{
-                  icon: Icons.menuDot,
-                  onPress: onPressList,
+                renderItem={({ item }) => {
+                  return (
+                    <ListItem
+                      viewableItems={viewableItems}
+                      item={item}
+                      key={Math.round(Math.random() * 1000).toString()}
+                    />
+                  );
                 }}
               />
-              {isLoading ? (
-                <View className="flex-1 items-center justify-center">
-                  <Chase size={50} color="rgba(255,255,255,0.5)" />
-                </View>
-              ) : (
-                <View className="flex-1 justify-center px-4 py-3">
-                  <Text className="font-DNNextLTB text-lg text-primary">
-                    {" "}
-                    الفصل الحالي : {Data?.SemesterName}
-                  </Text>
-                  <Text className="font-DNNextLT text-base text-gray-300">
-                    {" "}
-                    الفصل الدراسي الحالي : {Data?.Semester}
-                  </Text>
-                </View>
-              )}
-            </View>
-            <View
-              style={{
-                flex: 0.7,
-              }}
-              className="bg-primary"
-            >
-              <CategoriesDay
-                selectedDay={selectedDay}
-                onDayPress={handleDayPress}
-              />
-              {LecturesDay ? (
-                <FlatList
-                  data={LecturesDay}
-                  showsHorizontalScrollIndicator={false}
-                  contentContainerStyle={{ paddingTop: 40 }}
-                  onViewableItemsChanged={({ viewableItems: vItems }) => {
-                    viewableItems.value = vItems;
-                  }}
-                  renderItem={({ item }) => {
-                    return (
-                      <ListItem
-                        viewableItems={viewableItems}
-                        item={item}
-                        key={Math.round(Math.random() * 1000).toString()}
-                      />
-                    );
-                  }}
+            ) : (
+              <View className="flex-1 justify-center items-center p-5">
+                <Image
+                  source={Images.undraw_holiDay}
+                  contentFit="contain"
+                  className="w-[85%] h-[85%]"
                 />
-              ) : (
-                <View className="flex-1 justify-center items-center p-5">
-                  <Image
-                    source={Images.undraw_holiDay}
-                    contentFit="contain"
-                    className="w-[85%] h-[85%]"
-                  />
-                  <Text
-                  className="font-DNNextLTB text-lg text-gray-200"
-                  >
-                    لا يوجد محاضرات لليوم
-                  </Text>
+                <Text className="font-DNNextLTB text-lg text-gray-200">
+                  لا يوجد محاضرات لليوم
+                </Text>
+              </View>
+            )}
+
+            <ListOfCourses ref={ref}>
+              <View className="flex-1">
+                <View className="flex-row-reverse px-4 z-10">
+                  <View className="w-1/4 text-wrap">
+                    <Text className="font-DNNextLTB text-sm text-primary text-center">
+                      اسم المقرر الدراسي
+                    </Text>
+                  </View>
+                  <View className="w-1/4 text-wrap">
+                    <Text className="font-DNNextLTB text-sm text-primary text-center">
+                      رمز المقرر الدراسي
+                    </Text>
+                  </View>
+                  <View className="w-1/4 text-wrap">
+                    <Text className="font-DNNextLTB text-sm text-primary text-center">
+                      المجموعة النظري
+                    </Text>
+                  </View>
+                  <View className="w-1/4 text-wrap">
+                    <Text className="font-DNNextLTB text-sm text-primary text-center">
+                      المجموعة العملي
+                    </Text>
+                  </View>
                 </View>
-              )}
+                <View
+                  style={{ width, height }}
+                  className="absolute top-0 left-0 right-0"
+                >
+                  <Svg style={StyleSheet.absoluteFill} viewBox="0 0 375 100">
+                    <Path d={d} fill={"#f67d38"} />
+                  </Svg>
+                </View>
 
-              <ListOfCourses ref={ref}>
-                <View className="flex-1">
-                  <View className="flex-row-reverse px-4 z-10">
-                    <View className="w-1/4 text-wrap">
-                      <Text className="font-DNNextLTB text-sm text-primary text-center">
-                        اسم المقرر الدراسي
-                      </Text>
-                    </View>
-                    <View className="w-1/4 text-wrap">
-                      <Text className="font-DNNextLTB text-sm text-primary text-center">
-                        رمز المقرر الدراسي
-                      </Text>
-                    </View>
-                    <View className="w-1/4 text-wrap">
-                      <Text className="font-DNNextLTB text-sm text-primary text-center">
-                        المجموعة النظري
-                      </Text>
-                    </View>
-                    <View className="w-1/4 text-wrap">
-                      <Text className="font-DNNextLTB text-sm text-primary text-center">
-                        المجموعة العملي
-                      </Text>
-                    </View>
-                  </View>
-                  <View
-                    style={{ width, height }}
-                    className="absolute top-0 left-0 right-0"
-                  >
-                    <Svg style={StyleSheet.absoluteFill} viewBox="0 0 375 100">
-                      <Path d={d} fill={"#f67d38"} />
-                    </Svg>
-                  </View>
-
-                  <ScrollView
-                    contentContainerStyle={{
-                      height: "100%",
-                      alignItems: "center",
-                    }}
-                    showsVerticalScrollIndicator={false}
-                    style={{ marginTop: 20 }}
-                  >
-                    {Data ? (
-                      Data?.CurrentCourseDates?.map((item, index) => (
-                        <View
-                          className="w-full items-center"
-                          key={index.toString()}
-                        >
-                          <View className="w-full flex-row-reverse px-4 items-center">
-                            <View className="w-1/4 text-wrap">
-                              <Text className="font-DNNextLT text-lg text-black-200 text-center">
-                                {item.NameCourse}
-                              </Text>
-                            </View>
-                            <View className="w-1/4 text-wrap">
-                              <Text
-                                className="font-DNNextLT text-lg text-black-200 text-center"
-                                // How to reverse writing from left to right
-                                style={{ writingDirection: "rtl" }}
-                              >
-                                {item.CodeCourse}
-                              </Text>
-                            </View>
-                            <View className="w-1/4 text-wrap">
-                              <Text className="font-DNNextLT text-lg text-black-200 text-center">
-                                {item.GroupTheoretical ?? "-"}
-                              </Text>
-                            </View>
-                            <View className="w-1/4 text-wrap">
-                              <Text className="font-DNNextLT text-lg text-black-200 text-center">
-                                {item.GroupPractical ?? "-"}
-                              </Text>
-                            </View>
-                          </View>
-                          <View
-                            className="w-[90%] h-[2px] rounded-full px-2 my-2 bg-gray"
-                            key={Math.round(Math.random() * 100).toString()}
-                          />
-                        </View>
-                      ))
-                    ) : (
+                <ScrollView
+                  contentContainerStyle={{
+                    height: "100%",
+                    alignItems: "center",
+                  }}
+                  showsVerticalScrollIndicator={false}
+                  style={{ marginTop: 20 }}
+                >
+                  {Data ? (
+                    Data?.CurrentCourseDates?.map((item, index) => (
                       <View
-                        className="flex-1 absolute top-10 right-0 left-0
+                        className="w-full items-center"
+                        key={index.toString()}
+                      >
+                        <View className="w-full flex-row-reverse px-4 items-center">
+                          <View className="w-1/4 text-wrap">
+                            <Text className="font-DNNextLT text-lg text-black-200 text-center">
+                              {item.NameCourse}
+                            </Text>
+                          </View>
+                          <View className="w-1/4 text-wrap">
+                            <Text
+                              className="font-DNNextLT text-lg text-black-200 text-center"
+                              // How to reverse writing from left to right
+                              style={{ writingDirection: "rtl" }}
+                            >
+                              {item.CodeCourse}
+                            </Text>
+                          </View>
+                          <View className="w-1/4 text-wrap">
+                            <Text className="font-DNNextLT text-lg text-black-200 text-center">
+                              {item.GroupTheoretical ?? "-"}
+                            </Text>
+                          </View>
+                          <View className="w-1/4 text-wrap">
+                            <Text className="font-DNNextLT text-lg text-black-200 text-center">
+                              {item.GroupPractical ?? "-"}
+                            </Text>
+                          </View>
+                        </View>
+                        <View
+                          className="w-[90%] h-[2px] rounded-full px-2 my-2 bg-gray"
+                          key={Math.round(Math.random() * 100).toString()}
+                        />
+                      </View>
+                    ))
+                  ) : (
+                    <View
+                      className="flex-1 absolute top-10 right-0 left-0
                     justify-center items-center
                     p-8
                     "
-                      >
-                        <Image
-                          source={Images.undraw_empty}
-                          contentFit="contain"
-                          className="w-52 h-52"
-                        />
-                        <Text className="font-DNNextLT text-lg text-black-200 text-center mt-6">
-                          لا يوجد مواد في الوقت الحالي
-                        </Text>
-                      </View>
-                    )}
-                  </ScrollView>
-                </View>
-              </ListOfCourses>
-            </View>
+                    >
+                      <Image
+                        source={Images.undraw_empty}
+                        contentFit="contain"
+                        className="w-52 h-52"
+                      />
+                      <Text className="font-DNNextLT text-lg text-black-200 text-center mt-6">
+                        لا يوجد مواد في الوقت الحالي
+                      </Text>
+                    </View>
+                  )}
+                </ScrollView>
+              </View>
+            </ListOfCourses>
           </View>
-        </ScrollView>
+        </View>
+        {/* </ScrollView> */}
       </GestureHandlerRootView>
     </AlertNotificationRoot>
   );
